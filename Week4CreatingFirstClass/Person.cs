4ace204bc7c3cf5383d2bab524d40ef587baf346
Hamit Canpolat <hhamitc@gmail.com>
2024-10-18 13:00:48 +0300

    Add project files.

---
+++ Week4CreatingFirstClass/Person.cs
@@ -0,0 +1,68 @@
+﻿namespace Week4CreatingFirstClass
+{
+    internal class Person
+    {
+
+        // Alanları oluşturdum.
+
+        public string name;
+        public string surname;
+        public DateTime birthDate;
+        public string status;
+
+
+        public string Name // İsim alanının property lerinin okuma yazma işlemleri için metot tanımlyorum.
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Surname // Soyisim alanının property lerinin okuma yazma işlemleri için metot tanımlyorum.
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public DateTime BirthDate // Doğum tarihi alanının property lerinin okuma yazma işlemleri için metot tanımlyorum.
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            {
+                birthDate = value;
+            }
+        }
+        public string Status // Durum alanının property lerinin okuma yazma işlemleri için metot tanımlyorum.
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+
+        public void InformationWrite() // Aldığım verileri ekrana yazdırmak için bir metot oluşuturuyorum.
+        {
+            Console.WriteLine($"Adı: {name} , Soyadı; {surname}, Doğum Tarihi: {birthDate.ToString("dd/mm/yyyy")}; Sınıftaki Konumu: {status}");
+        }
+
+
+
+    }
+}
---
+++ Week4CreatingFirstClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using Week4CreatingFirstClass;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Person person1 = new Person();
+
+        // Person sınıfımın nesnelerini oluşturuyorum.
+
+        person1.name = "Hamit";
+        person1.surname = "Canpolat";
+        person1.birthDate = new DateTime(1991, 08, 23);
+        person1.status = "Öğrenci";
+
+
+        Person person2 = new Person();
+        person2.name = "Atakan";
+        person2.Surname = "Gürdal";
+        person2.birthDate = new DateTime(1996, 05, 02);
+        person2.status = "Öğrenci";
+
+        Person person3 = new Person();
+        person3.name = "Gizem";
+        person3.Surname = "Güneş";
+        person3.birthDate = new DateTime(1996, 10, 16);
+        person3.status = "Öğrenci";
+
+        Person person4 = new Person();
+        person4.name = "Yiğit";
+        person4.surname = "Hacıefendioğlu";
+        person4.birthDate = new DateTime(1985, 07, 28);
+        person4.status = "Öğretmen";
+
+
+        // Nesneleri metotla çağırıp ekrana yazdırıyorum.
+
+        person1.InformationWrite();
+        person2.InformationWrite();
+        person3.InformationWrite();
+        person4.InformationWrite();
+
+
+
+
+        Console.ReadKey();
+    }
+}

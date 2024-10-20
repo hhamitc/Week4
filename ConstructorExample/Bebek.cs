ae626628187aef3ded20336ee1b37fd20db872f2
Hamit Canpolat <hhamitc@gmail.com>
2024-10-21 00:50:17 +0300

    Bebek sınıfı oluşturuldu.

---
+++ ConstructorExample/Bebek.cs
@@ -0,0 +1,49 @@
+﻿namespace ConstructorExample
+{
+    internal class Bebek
+    {
+
+        //Created my fields
+        public string _name;
+        public string _surname;
+        public DateTime _birthDate;
+
+
+
+        //Set my properties for my fileds
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public DateTime BirthDate { get { return _birthDate; } set { _birthDate = value; } }
+
+        //Creating default constructor method
+        public Bebek()
+        {
+            _birthDate = DateTime.Now;
+            FirstCry();
+        }
+
+        //Creating alternative constructor method
+        public Bebek(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            _birthDate = DateTime.Now;
+            FirstCry();
+
+        }
+
+        //Giving output for baby information
+        public void BabyInfo()
+        {
+            Console.WriteLine($"Bebek dünyaya geldi!! \nBebeğin Bilgileri: ");
+            Console.WriteLine($"Adı: {_name}, \nSoyadı: {_surname}, \nDoğum Zamanı: {_birthDate}");
+        }
+
+        //Baby crying
+        public void FirstCry()
+        {
+            Console.WriteLine("Ingaaaa!!!");
+        }
+
+    }
+}
---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConstructorExample;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Creating a baby object with default constructor
+
+        Bebek baby1 = new Bebek();
+
+        baby1.Name = "Ayşe"; // Setting name and surname.
+        baby1.Surname = "Kiraz";
+        baby1.BabyInfo();
+
+
+        Console.WriteLine(new string('-', 33));
+
+
+        //Creatin another baby object with alternative constructor
+        Bebek baby2 = new Bebek("Hakan", "Kaçan");
+        baby2.BabyInfo();
+
+
+
+
+
+
+
+
+        Console.ReadKey();
+
+    }
+}

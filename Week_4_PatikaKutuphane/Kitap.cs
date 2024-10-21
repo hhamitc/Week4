dc0066c52f7adb79a058e0dca28bfad683a3a3e6
Hamit Canpolat <hhamitc@gmail.com>
2024-10-21 14:26:55 +0300

    Week 4 Kütüphaneye kitap ekleme örneği.

---
+++ Week_4_PatikaKutuphane/Kitap.cs
@@ -0,0 +1,63 @@
+﻿namespace Week_4_PatikaKutuphane
+{
+    internal class Kitap
+    {
+
+
+
+        // Creating my fields 
+        public string _bookName;
+        public string _authorName;
+        public string _authorSurname;
+        public int _pageCount;
+        public string _publisher;
+        public DateTime _recordDate;
+
+
+        //Setting my properties of my fields
+        public string BookName { get { return _bookName; } set { _bookName = value; } }
+        public string AuthorName { get { return _authorName; } set { _authorName = value; } }
+        public string AuthorSurname { get { return _authorSurname; } set { _authorSurname = value; } }
+        public int PageCount { get { return _pageCount; } set { _pageCount = value; } }
+        public string Publisher { get { return _publisher; } set { _publisher = value; } }
+
+        //Creating default constructor method
+        public Kitap()
+        {
+            _bookName = "Girilmedi. ";
+            _authorName = "Girilmedi";
+            _authorSurname = "Girilmedi";
+            _pageCount = 0;
+            _publisher = "Girilmedi";
+            _recordDate = DateTime.Now;
+        }
+
+        //Creating alternative constructor method
+        public Kitap(string bookName, string authorName, string authorSurname, int pageCount, string publisher)
+        {
+            _bookName = bookName;
+            _authorName = authorName;
+            _authorSurname = authorSurname;
+            _pageCount = pageCount;
+            _publisher = publisher;
+            _recordDate = DateTime.Now;
+        }
+
+        //Giving output message for book info
+        public void BookInfo()
+        {
+            Console.WriteLine($"Kitap Adı: {_bookName}\nYazar Adı- Soyadı: {_authorName} {_authorName}\nSayfa Sayısı: {_pageCount}\nYayın Evi: {_publisher}\nKayıt tarihi: {_recordDate}");
+            Console.WriteLine(new String('-', 33));
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Week_4_PatikaKutuphane/Program.cs
@@ -0,0 +1,51 @@
+﻿using Week_4_PatikaKutuphane;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Creating a book object with default counstructor 
+        Kitap kitap1 = new Kitap();
+
+        kitap1.BookName = "Adı Aylin";
+        kitap1.AuthorName = "Ayşe;";
+        kitap1.AuthorSurname = "Kulin;";
+        kitap1.PageCount = 398;
+        kitap1.Publisher = "Remzi Kitabevi";
+
+
+        kitap1.BookInfo();//Printing book informations to console.
+
+
+        // Creating a book object with alternative counstructor 
+
+
+        Kitap kitap2 = new Kitap("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+
+        kitap2.BookInfo();
+
+
+        // Creating a book object with default counstructor wtitout any information
+        Kitap kitap3 = new Kitap();
+        kitap3.BookInfo();
+
+
+
+
+
+        // Class bir nesnenin yapısını ve davranışlarını tanımlayan bir şablondur. Bu örnekte Kitap adında bir sınıf oluşturduk. Bu sınıf, kitap nesnelerini temsil ediyor ve kitapla ilgili özellikleri (property) ve davranışları (metotlar) içeriyor.
+
+        //Property, sınıfın içindeki verileri temsil eder. Kitap sınıfı içinde, kitabın adı, yazarı, sayfa sayısı, yayınevi ve kayıt tarihi gibi bilgileri tutan property'ler tanımlandı. Bu özelliklere dışarıdan erişilebilir ve değiştirilebilir.
+
+        //New anahtar kelimesi, bir sınıftan yeni bir nesne yaratmak için kullanılır. Bu işlemle sınıfın bir örneği (instance) oluşturulur. Örneğin, kitap1 ve kitap2 isimli iki ayrı kitap nesnesi oluşturduk.
+
+        //Constructor, bir sınıftan yeni bir nesne oluşturulurken ilk olarak çalışan özel bir metottur. Constructor, nesnenin başlangıç durumunu ayarlamaya yarar. Örnekte, iki tür constructor kullandık: biri parametre almayan (default constructor), diğeri ise kitapla ilgili bilgileri parametre olarak alıp, nesne yaratıldığında bu bilgileri property'lere atayan bir constructor'dır.
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}

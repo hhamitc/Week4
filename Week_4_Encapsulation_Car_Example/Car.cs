c90c02c1745b0a29993d08d51bdee9cc0ff599ba
Hamit Canpolat <hhamitc@gmail.com>
2024-10-21 21:29:36 +0300

    Enheritance ödevi yapıldı.

---
+++ Week_4_Encapsulation_Car_Example/Car.cs
@@ -0,0 +1,58 @@
+﻿namespace Week_4_Encapsulation_Car_Example
+{
+    public class Car
+    {
+        //Marka, Model, Renk, Kapı Sayısı
+
+        public string _brand;
+        public string _modelName;
+        public string _color;
+        private int _doorCount;
+
+        public Car(string brand, string modelName, string color, int doorCount)
+        {
+            _brand = brand;
+            _modelName = modelName;
+            _color = color;
+            DoorCount = doorCount;
+        }
+
+        public string Brand { get { return _brand; } set { _brand = value; } }
+
+        public string ModelName { get { return _modelName; } set { _modelName = value; } }
+
+        public string Color { get { return _color; } set { _color = value; } }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı kapı sayısı değeri!!");
+                    _doorCount = -1;
+                }
+            }
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Marka: {_brand}\nModel: {_modelName}\nRenk: {_color}\nKapı Sayısı: {_doorCount} ");
+            Console.WriteLine(new String('-', 33));
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Week_4_Encapsulation_Car_Example/Program.cs
@@ -0,0 +1,34 @@
+﻿using Week_4_Encapsulation_Car_Example;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+        Bu örnekte sizlerden bir Araba sınıfı oluşturmanız isteniyor.
+
+        Araba sınıfının özellikleri -> Marka, Model, Renk, Kapı Sayısı
+
+        Özelliklerimizden (Property) Kapı sayısı için kapsülleme işlemi yapmak istiyoruz.
+
+        Eğer bir Araba nesnesi oluşturulduğunda Kapı Sayısı için 2 veya 4 dışında bir değer atanmak isterse konsol ekranında bir uyarı mesajı yayınlayalım ve Kapı Sayısı değerini -1 olarak atayalım.
+
+         */
+
+        Car car1 = new Car("Mazda", "Mx-5", "Red", 2); // Creating new objet from car Class
+        car1.DisplayCarInfo();                          //Displaying car info on console.
+
+        Car car2 = new Car("Honda", "Civic Type-R", "Red", 3);// Creating new objet from car Class
+        car2.DisplayCarInfo();                              //Displaying car info on console.
+
+
+
+
+
+
+
+
+
+        Console.ReadKey();
+    }
+}

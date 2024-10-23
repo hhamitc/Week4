3b6c4ad9667ab51a177239fbf7c2d9a587dea76b
Hamit Canpolat <hhamitc@gmail.com>
2024-10-23 13:23:09 +0300

    Abstract ödevi yapıldı.

---
+++ Week_4_ Abstraction_Practice/Employee.cs
@@ -0,0 +1,83 @@
+﻿namespace Week_4__Abstraction_Practice
+{
+    public abstract class Employee
+    {
+        /*Creating the properties, and there is no need to define a
+        department since the department information will be automatically
+        derived from the class in which it is created.*/
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+
+        //Creating consructor for make data entry mandatory
+        public Employee(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+
+        //We are defining our method as abstract so that it can be customized for each department.
+        public abstract void DisplayPosition();
+    }
+
+    //Derivig department classes based on Employee
+    public class SoftwareDeveloper : Employee
+    {
+        public SoftwareDeveloper(string name, string lastName) : base(name, lastName)
+        {
+        }
+
+        //Displaying Employee info
+
+        public override void DisplayPosition()
+        {
+            Console.WriteLine($"{Name} {LastName} working as SoftwareDeveloper.");
+
+        }
+    }
+
+    //Derivig department classes based on Employee
+
+    public class ProjectManager : Employee
+    {   //Creating constructor for sending base class
+        public ProjectManager(string name, string lastName) : base(name, lastName)
+        {
+
+        }
+        //Displaying Employee info
+        public override void DisplayPosition()
+        {
+            Console.WriteLine($"{Name} {LastName} working as Project Manager.");
+        }
+    }
+
+    //Derivig department classes based on Employee
+
+    public class Salesman : Employee
+    {
+        //Creating constructor for sending base class
+
+        public Salesman(string name, string lastName) : base(name, lastName)
+        {
+
+        }
+
+        //Displaying Employee info
+
+        public override void DisplayPosition()
+        {
+            Console.WriteLine($"{Name} {LastName} working as Salesman.");
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
+
---
+++ Week_4_ Abstraction_Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using Week_4__Abstraction_Practice;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Creaeting class objects
+        //And Calling informations.
+
+        SoftwareDeveloper sDev1 = new SoftwareDeveloper("Hamit", "Canpolat");
+
+        sDev1.DisplayPosition();
+
+        ProjectManager pManager1 = new ProjectManager("Hasan", "Çildirmiş");
+
+        pManager1.DisplayPosition();
+
+        Salesman salesman = new Salesman("Türkan", "Mavi");
+
+        salesman.DisplayPosition();
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
